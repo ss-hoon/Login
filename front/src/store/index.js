4e9f12d62bd6d15cbb25a26eacc941b034c1f64b
ss-hoon <46196555+ss-hoon@users.noreply.github.com>
2021-06-23 23:17:34 +0900

    vuex 추가

--- front/src/store/index.js
+++ front/src/store/index.js
@@ -5,11 +5,33 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    userInfo: {
+      userId: "",
+      userPwd: ""
+    }
+  },
+  getters: {
+    getUserId: state => {
+      return state.userInfo.userId;
+    },
+    getUserPwd: state => {
+      return state.userInfo.userPwd;
+    }
   },
   mutations: {
+    setUserId: (state, payload) => {
+      state.userInfo.userId = payload.userId;
+    },
+    setUserPwd: (state, payload) => {
+      state.userInfo.userPwd = payload.userPwd;
+    }
   },
   actions: {
-  },
-  modules: {
+    setUserId: ({ commit }, payload) => {
+      commit("setUserId", payload);
+    },
+    setUserPwd: ({ commit }, payload) => {
+      commit("setUserPwd", payload);
+    }
   }
 })
