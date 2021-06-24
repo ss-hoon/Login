2d266cbc64aa655d5429a9a9476a240c7325c153
ss-hoon <46196555+ss-hoon@users.noreply.github.com>
2021-06-24 23:16:35 +0900

    ID, PW vuex 연동

--- front/src/components/login/InputID.vue
+++ front/src/components/login/InputID.vue
@@ -2,11 +2,26 @@
     <div class="inputId">
         <v-text-field
             label="ID"
+            v-model="userId"
         ></v-text-field>
     </div>
 </template>
+
 <script>
     export default {
-
+        name: "InputID",
+        data() {
+            return {
+                userId: ""
+            }
+        },
+        created() {
+            this.userId = this.$store.getters.getUserId;
+        },
+        watch: {
+            userId() {
+                this.$store.dispatch("setUserId", this.userId);
+            }
+        }
     }
 </script>
--- front/src/components/login/InputPW.vue
+++ front/src/components/login/InputPW.vue
@@ -2,12 +2,26 @@
     <div class="inputPw">
         <v-text-field
             label="Password"
+            v-model="userPwd"
         ></v-text-field>
     </div>
 </template>
 
 <script>
     export default {
-        
+        name: "InputPW",
+        data() {
+            return {
+                userPwd: ""
+            }
+        },
+        created() {
+            this.userPwd = this.$store.getters.getUserId;
+        },
+        watch: {
+            userId() {
+                this.$store.dispatch("setUserPwd", this.userPwd);
+            }
+        }
     }
 </script>
