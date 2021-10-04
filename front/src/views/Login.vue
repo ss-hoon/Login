7f34640ab787a96b833393fd6683720f4870ceee
ss-hoon <https://github.com/heung27/ADRECO.git>
2021-10-04 18:15:08 +0900

    소셜 로그인 버튼 추가

--- front/src/views/Login.vue
+++ front/src/views/Login.vue
@@ -40,9 +40,20 @@
         <div class="signUp">
             <v-btn id="signUp">회원가입</v-btn>
         </div>
+
+        <div class="hr-sns">소셜 로그인</div>
+
+        <div class="snsLogin">
+            <img src="@/assets/imgs/naver_login.png" width="40px" height="40px"/>
+            <img src="@/assets/imgs/kakao_login.png" width="40px" height="40px"/>
+            <img src="@/assets/imgs/google_login.svg" width="40px" height="40px"/>
+            <img src="@/assets/imgs/apple_login.png" width="40px" height="40px"/>
+        </div>
     </div>
 </template>
 <script>
+import axios from "axios";
+
 export default {
     name: 'Login',
     components: {
@@ -70,10 +81,47 @@ export default {
     created() {
         
     },
+    mounted() {
+        var self = this; 
+        try{
+            if(this.$route.query.code.length !== undefined){
+                var callbackFuc = async () => {
+                    const res = await fetch('https://localhost/api/sns_login_naver', { 
+                        method: "POST", headers: {
+                               "Content-Type": "application/json", 
+                        }, body: JSON.stringify({
+                               code: `${self.$route.query.code}`,
+                               state: `${self.$route.query.state}`
+                        }),
+                    });
+                    
+                    const data = await res.json();
+                    console.log(`네이버 로그인 : ${data.email}`)
+                } 
+                callbackFuc(); 
+            } 
+        }catch (e){ 
+            console.log(e);
+        }
+    },
     methods: {
-        onClickLogin() {
+        async onClickLogin() {
             console.log(`로그인 ID : ${this.$store.getters.getUserId}`);
             console.log(`로그인 PWD : ${this.$store.getters.getUserPwd}`);
+
+            const param = {
+                userId: this.loginId,
+                userPwd: this.loginPwd
+            };
+
+            await axios
+                .post("api/authentication/login", param)
+                .then((res) => {
+                    console.log(res);
+                })
+                .catch((error) => {
+                    console.log(error);
+                })
         }
     },
     watch: {
